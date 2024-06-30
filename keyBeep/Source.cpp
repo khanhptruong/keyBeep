e1e878de29a0b35bed5c4aededdce70c96439381
Khanh Truong <khanht93@gmail.com>
2024-06-29 22:17:53 -0700

    Added scan code and separate key down/up.
    Separate key down from key up.
    Now send key scan code as well. Some apps (Supreme Commander 2) rely on hardware scan code.

--- keyBeep/Source.cpp
+++ keyBeep/Source.cpp
@@ -2,63 +2,49 @@
 #include <iostream>
 #include <WinUser.h>
 
-void replaceKey(DWORD replacementKey)
+enum KeyEdge { UP, DOWN };
+
+void replaceKey(DWORD replacementKey, WORD scanKey, KeyEdge kEdge)
 {
-	INPUT inputs[2] = {};
+	INPUT inputs[1] = {};
 	ZeroMemory(inputs, sizeof(inputs));
 
 	inputs[0].type = INPUT_KEYBOARD;
 	inputs[0].ki.wVk = replacementKey;
-
-	inputs[1].type = INPUT_KEYBOARD;
-	inputs[1].ki.wVk = replacementKey;
-	inputs[1].ki.dwFlags = KEYEVENTF_KEYUP;
-
+	inputs[0].ki.wScan = scanKey;
+	//if kEdge is DOWN, don't set dwFlag
+	if (kEdge == UP) { inputs[0].ki.dwFlags = KEYEVENTF_KEYUP; }
 
 	UINT uSent = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
-	if (uSent != ARRAYSIZE(inputs))
-	{
-		std::cout << "SendInput failed: 0x%x" << std::endl;
-	}
+	if (uSent != ARRAYSIZE(inputs)) { std::cout << "SendInput failed: 0x%x" << std::endl; }
 }
 
 LRESULT CALLBACK KBDHook(int nCode, WPARAM wParam, LPARAM lParam) {
 	KBDLLHOOKSTRUCT* s = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
+	KeyEdge kEdge;
 
-	if (wParam == WM_KEYDOWN)
+	if (wParam == WM_KEYDOWN)    { kEdge = DOWN; }
+	else if (wParam == WM_KEYUP) { kEdge = UP; }
+	else						 { return CallNextHookEx(NULL, nCode, wParam, lParam); }
+
+	switch (s->vkCode)
 	{
-		//char c = MapVirtualKey(s->vkCode, MAPVK_VK_TO_CHAR);
-
-		/*
-		Note for PlaySound:
-
-		Need to link winmm.lib library to use PlaySound.
-		Righ Click Project > properties > linker > input < add ;winmm.lib at the end
-
-		https://stackoverflow.com/questions/21034935/playsound-in-c
-		*/
-
-		switch (s->vkCode) {
-		case 0x57: //W
-			replaceKey(VK_UP);
-			return 1; //if key is replaced, dont forward original key along
-			break;
-		case 0x41: //A
-			replaceKey(VK_LEFT);
-			return 1; //if key is replaced, dont forward original key along
-			break;
-		case 0x53: //S
-			replaceKey(VK_DOWN);
-			return 1; //if key is replaced, dont forward original key along
-			break;
-		case 0x44: //D
-			replaceKey(VK_RIGHT);
-			return 1; //if key is replaced, dont forward original key along
-			break;
-		default: //all other keys, forward along as usual
-			break;
-		}
+	case 0x57: //W
+		replaceKey(VK_UP, 72, kEdge);
+		return 1; //if key is replaced, dont forward original key along
+	case 0x41: //A
+		replaceKey(VK_LEFT, 75, kEdge);
+		return 1; //if key is replaced, dont forward original key along
+	case 0x53: //S
+		replaceKey(VK_DOWN, 80, kEdge);
+		return 1; //if key is replaced, dont forward original key along
+	case 0x44: //D
+		replaceKey(VK_RIGHT, 77, kEdge);
+		return 1; //if key is replaced, dont forward original key along
+	default: //all other keys, forward along as usual
+		break;
 	}
+
 	return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
